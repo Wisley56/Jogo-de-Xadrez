49f784408b7a16faaa8e6fec398cda3093ab6f58
Wisley <fwisley56gmail.com>
2023-07-22 15:23:14 -0300

    Tratando erros com exceções personalizadas

--- Board/Tray.cs
+++ Board/Tray.cs
@@ -13,14 +13,37 @@ namespace Board
             this.Columns = columns;
             pieces = new Piece[Lines, Columns];
         }
-        public Piece getPiece(int line, int column)
+        public Piece piece(int line, int column) //retorna a peça na posicao desejada
         {
             return pieces[line, column];
+        }
+        public Piece getPiece( Position pos) //retorna a peça na posicao atual
+        {
+            return pieces[pos.Line, pos.Columns];
         }
         public void changePiece(Piece p, Position pos) // muda a peça de lugar
         {
             pieces[pos.Line, pos.Columns] = p;
             p.Position = pos;
         }
+        public bool validPosition(Position pos) //verificando se a posiçao é valida
+        {
+            if(pos.Line < 0 || pos.Line >= Lines || pos.Columns < 0 || pos.Columns >= Columns)
+            {
+                return false;
+            }
+            return true;
+        }
+        public void validatePosition(Position pos) //validar a posiçao usando exceçao
+        {
+            if(!validPosition(pos))
+                throw new TrayExceptions("Invalid Position!");
+        }
+        public bool existPiece(Position pos) //verifica se existe a peça
+        {
+            validatePosition(pos);
+            return getPiece(pos) != null;
+        }
+
     }
 }
---
+++ Board/TrayExceptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace Board
+{
+    internal class TrayExceptions : Exception
+    {
+        public TrayExceptions(string msg) : base(msg)
+        {
+            
+        }
+    }
+}
