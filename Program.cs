56f78aaf43f77dff610eb153a4818fc102750a5c
Wisley <fwisley56gmail.com>
2023-07-22 11:26:15 -0300

    Projeto criado

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Xadrez___Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
